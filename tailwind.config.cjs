1b00de06c5e7f1185a6372dcea532c2600dcf8fc
Tsaqif Neoclovin <neoclovint@gmail.com>
2026-09-12 16:41:59 +0700

    Add safelist for glass classes in Tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  safelist: [
+    {
+      pattern: /glass-(card|pill|button)/,
+    },
+  ],
   theme: {
     extend: {},
   },
